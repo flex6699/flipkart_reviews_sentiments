ede4eb784607a833cbf86ae97a3fe1107ab2fafa
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-07-03 00:03:12 +0530

    upgrade

--- app.py
+++ app.py
@@ -89,7 +89,6 @@ template = """
     {query}
 
     Please provide your analysis in one word: "positive", "negative", or "neutral." I need answers only in "positive", "negative", or "neutral".
-    "only provide answer in positive ,negative or neutral only one word"
 
     Thank you.
     """
