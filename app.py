f2a53669d8d37edf6fde0d2ae7e90fe4e09d5245
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-07-06 01:49:02 +0530

    v2

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def scrape_reviews(url, max_page):
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
+    options.add_argument("--disable-dev-shm-usage")
     driver = get_driver()
     for i in range(1, max_page + 1):
         page_url = f"{url}&page={i}"
